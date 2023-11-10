9d8f82959413372b3ac27e531756bb7977012848
Matthew Lamour <68308905+itsxodus@users.noreply.github.com>
2023-11-10 16:04:16 -0500

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,30 +1,34 @@
 async function base64ImageData(imageUrl) {
   try {
-        const response = await fetch(imageUrl);
-        const blob = await response.blob();
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
     
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.onloadend = () => resolve(reader.result);
-            reader.onerror = reject;
-            reader.readAsDataURL(blob);
-        });
-    } catch (error) {
-        console.error('Error converting image to base64:', error);
-        return null;
-    }
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+    });
+   } catch (error) {
+       console.error('Error converting image to base64:', error);
+       return null;
+   }
 }
 
 function getImageData () {
   const images = document.querySelectorAll('img');
+  let base64ImageDataArray = [];
   
   images.forEach((image, index) => {
     base64ImageData(image.src).then(base64Data => {
         if (base64Data) {
+            base64ImageDataArray.push(base64Data);
             console.log(`Image ${index + 1}: ${image.src}, (${base64Data})`);
         } else {
             console.log(`Image ${index + 1}: ${image.src}, (Failed to convert image to base64.)`);
         }
     });
   });
+  console.log('Base 64 Image Data Array (the array itself is also returned by this function): ', base64ImageDataArray);
+  return base64ImageDataArray;
 }
